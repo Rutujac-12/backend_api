3e3ba0a77694d52788bb33469ef2e36518da384c
Chaudhari Rutuja Durgesh <rutujachaudhari41@gmail.com>
2024-04-04 22:29:09 +0530

    second commit

---
+++ Routes/userRoutes.js
@@ -0,0 +1,13 @@
+
+import express from 'express'
+import { homeController, signup, getUser } from '../controllers/user.js';
+
+const router = express.Router();
+
+router.get('/', homeController);
+
+router.get('/getuser/:userid', getUser);
+
+router.post('/signup', signup);
+
+export default router;
